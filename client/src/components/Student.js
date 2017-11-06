29c97edd7e555e45e01668317a2da4cea7251dff
Dean Radcliffe <deanmisc@yahoo.com>
2017-11-05 21:14:37 -0600

    Add some fun images. Closes #37

--- client/src/components/Student.js
+++ client/src/components/Student.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
-import logo from './trilobyte.svg';
-import './App.css';
+import './Student.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src="/img/trilobyte.jpg" className="App-logo" alt="logo" />
           <h2>Welcome to Trilobytes!</h2>
         </div>
         <p className="App-intro">
