64000d6ce33a633297771d2fa4ab402a70fab085
Dean Radcliffe <deanmisc@yahoo.com>
2017-12-03 20:22:33 -0600

    Adds markdown to all the things

--- client/src/components/Student.js
+++ client/src/components/Student.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import "./Student.css";
 import VoteButton from "./VoteButton";
 import JoinForm from "./JoinForm";
+import ReactMarkdown from "react-markdown";
 
 class Student extends Component {
   handleAnswer = choice => {
@@ -22,6 +23,7 @@ class Student extends Component {
       <div>
         <div>Question {questionKey}</div>
         <h2>{prompt}</h2>
+        <ReactMarkdown source={round.markup} />
         <br />
         <div className="voting">
           {choices.map(choice => {
