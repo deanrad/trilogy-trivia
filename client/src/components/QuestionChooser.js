7e56a5ed6f579579bbcc5a347a179b4bd70a376b
Dean Radcliffe <deanmisc@yahoo.com>
2017-12-28 08:53:22 -0600

    Added category selection UI Issue #42

--- client/src/components/QuestionChooser.js
+++ client/src/components/QuestionChooser.js
@@ -1,5 +1,7 @@
 import React from "react";
 import axios from "axios";
+import Select from "react-select";
+import "react-select/dist/react-select.css";
 
 const allQuestionStyles = {
   border: "1px solid black",
@@ -9,21 +11,27 @@ const allQuestionStyles = {
 
 export const DataQuestionChooser = class DataQuestionChooser extends React.Component {
   state = {
-    questions: []
+    questions: [],
+    categories: new Set()
   };
   componentDidMount() {
-    axios
-      .get("/questions.json")
-      .then(({ data: questions }) => this.setState({ questions }));
+    const categories = new Set();
+    axios.get("/questions.json").then(({ data: questions }) => {
+      questions.forEach(q => {
+        (q.categories || []).forEach(cat => categories.add(cat));
+      });
+      this.setState({ questions, categories });
+    });
   }
   render() {
-    return <QuestionChooser questions={this.state.questions} />;
+    return <QuestionChooser {...this.state} />;
   }
 };
 
 export default class QuestionChooser extends React.Component {
   state = {
-    selected: []
+    selected: [],
+    selectedCats: null
   };
 
   toggleSelection(q) {
@@ -38,26 +46,41 @@ export default class QuestionChooser extends React.Component {
     });
   }
 
+  handleCatSelect = selectedCats => {
+    this.setState({ selectedCats });
+  };
+
   render() {
     let { questions = [] } = this.props;
-
+    let options = Array.from(this.props.categories).map(cat => ({
+      label: cat,
+      value: cat
+    }));
     return (
-      <div style={allQuestionStyles}>
-        {questions.map(q => {
-          const isSelected = this.state.selected.includes(q)
-          return (
-            <div
-              key={q.questionKey}
-              onClick={() => {
-                this.toggleSelection(q);
-              }}
-              className={isSelected ? 'selected' : ''}
-            >
-              <dt>{q.questionKey}</dt>
-              <dd>{q.prompt}</dd>
-            </div>
-          );
-        })}
+      <div>
+        <Select
+          multi={true}
+          options={options}
+          value={this.state.selectedCats}
+          onChange={this.handleCatSelect}
+        />
+        <div style={allQuestionStyles}>
+          {questions.map(q => {
+            const isSelected = this.state.selected.includes(q);
+            return (
+              <div
+                key={q.questionKey}
+                onClick={() => {
+                  this.toggleSelection(q);
+                }}
+                className={isSelected ? "selected" : ""}
+              >
+                <dt>{q.questionKey}</dt>
+                <dd>{q.prompt}</dd>
+              </div>
+            );
+          })}
+        </div>
       </div>
     );
   }
